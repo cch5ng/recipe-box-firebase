c3c66abb38662419a4d931c1d22cfeb323f16984
Carol Chung <cch5ng@gmail.com>
2017-03-03 22:11:56 -0800

    Anim/#17 (#19)
    * style: #17; partial implementation of hover in name
    
    * style: title anim, layout
    
    * style: #17
    
    * fix: #17; layout

--- app/components/Recipe.jsx
+++ app/components/Recipe.jsx
@@ -80,11 +80,13 @@ export default class Recipe extends React.Component {
 		})
 
 		recipeClass = classStrOutter + ' col-xs-12 col-sm-2 col-md-3';
+//		<p className="h4" onClick={this.toggleIngredients}>{name}</p>
 
 		return (
-			<div className="col-xs-12 col-sm-6 col-md-2 col-lg-4" key={this.state.key}>
-				<p className="h4" onClick={this.toggleIngredients}>{name}</p>
-				<img src={this.props.image_url} className="img-responsive" alt="photo" />
+			<div className="col-xs-12 col-sm-6 col-md-6 col-lg-4" key={this.state.key}>
+				<a href="#" className="title" alt="ice cream"><img src={this.props.image_url} className="img-responsive" alt="photo" />
+					<div className="hover-title">{name}</div>
+				</a>
 				<div className={classStr}>
 					<p className="h5">Ingredients</p>
 					<div className="ingredientList">
